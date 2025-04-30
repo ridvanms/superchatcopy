e322d4c641e142540f36fe0c3ecadf961c92eb3a
ridvanms <ridvans@abv.bg>
2025-04-30 14:20:21 +0300

    refactor: replace hardcoded Firebase config with environment variables

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import {useCollectionData} from 'react-firebase-hooks/firestore'
 import {useAuthState} from 'react-firebase-hooks/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDagLFKtNqQeUQiLDDpga1qbUtRvnKb6fc",
-  authDomain: "superchat-cd120.firebaseapp.com",
-  projectId: "superchat-cd120",
-  storageBucket: "superchat-cd120.firebasestorage.app",
-  messagingSenderId: "626329271484",
-  appId: "1:626329271484:web:fd6982460de9246c6e64b9"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 }
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
